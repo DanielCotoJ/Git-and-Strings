6096bd0a0a35a1d0d7567608c9f6aaea92560f00
Daniel Coto Jimenez <dannyc0902@gmail.com>
2025-03-14 14:39:14 -0600

    feat

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,5 +1,9 @@
-export default function Guitar({ guitar }) {
+export default function Guitar({ guitar, setCart }) {
   const { id, name, price, description, image } = guitar;
+
+  const handleClick = (guitar) => {
+    setCart(guitar);
+  };
 
   return (
     <>
@@ -16,7 +20,11 @@ export default function Guitar({ guitar }) {
           <p>{description}</p>
           <p className="fw-black text-primary fs-3">${price}</p>
 
-          <button type="button" className="btn btn-dark w-100">
+          <button
+            type="button"
+            className="btn btn-dark w-100"
+            onClick={() => handleClick(guitar)}
+          >
             Agregar al Carrito
           </button>
         </div>
